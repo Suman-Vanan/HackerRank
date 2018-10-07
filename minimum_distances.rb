ae8711f0680f030e965ad5e646884d0f9dda8a4e
Suman Vanan <suman.vanan@live.com>
2018-10-07 18:55:56 +0800

    Static code analysis changed some stuff

--- minimum_distances.rb
+++ minimum_distances.rb
@@ -5,26 +5,26 @@ require 'stringio'
 
 # Complete the minimumDistances function below.
 def minimumDistances(a)
-    
-    min_distance_arr = []
-    
-    unique_values = a.uniq
-    unique_values.each do |num| 
-        if a.count(num) <= 1
-            next
-        else
-            index_matches = a.each_index.select{|i| a[i] == num} # =>[0, 2, 6]
-           min_index_pair = index_matches.combination(2).min_by { |a,b| (a-b).abs }
-            min_distance_arr << (min_index_pair.first - min_index_pair.last).abs
-        end    
-        
+
+  min_distance_arr = []
+
+  unique_values = a.uniq
+  unique_values.each do |num|
+    if a.count(num) <= 1
+      next
+    else
+      index_matches = a.each_index.select{|i| a[i] == num} # =>[0, 2, 6]
+      min_index_pair = index_matches.combination(2).min_by { |a,b| (a-b).abs }
+      min_distance_arr << (min_index_pair.first - min_index_pair.last).abs
     end
-    
-    if min_distance_arr.length ==  0
-        return -1
-    else 
-        return min_distance_arr.sort.first
-    end
+
+  end
+
+  if min_distance_arr.length.zero?
+    -1
+  else
+    min_distance_arr.min
+  end
 end
 
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
