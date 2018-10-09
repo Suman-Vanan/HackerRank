819694a3c0b5ff516156c8ed9d44918c5ca54867
Suman Vanan <suman.vanan@live.com>
2018-10-09 11:46:04 +0800

    Non-Divisible subset working partially, fails some test cases

---
+++ Non-Divisible_Subset.rb
@@ -0,0 +1,48 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the nonDivisibleSubset function below.
+def nonDivisibleSubset(k, s)
+  potentialSubsets = []
+  for i in 2..s.length do
+    subsetArray = s.combination(i).to_a
+    subsetArray.each { |x| potentialSubsets << x}
+  end
+  potentialSubsets.reverse!
+  
+  for subset in potentialSubsets do
+    if validSubset(subset, k)
+      return subset.length
+    end
+  end
+  return 0
+end
+
+def validSubset(subset, k)
+  pairArray = subset.combination(2).to_a
+  pairArray.each { |pair| 
+    if pair.sum % k == 0
+      return false
+    end
+  }
+  return true
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+nk = gets.rstrip.split
+
+n = nk[0].to_i
+
+k = nk[1].to_i
+
+S = gets.rstrip.split(' ').map(&:to_i)
+
+result = nonDivisibleSubset k, S
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
