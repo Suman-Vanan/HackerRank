3ce3c9a6d5fc46ad5dcaf56ac48ee88534763bf7
Suman Vanan <suman.vanan@live.com>
2018-10-11 17:56:14 +0800

    Updated solution to handle strings of length 1

--- Strings/Highest Value Palindrome/solution.rb
+++ Strings/Highest Value Palindrome/solution.rb
@@ -5,48 +5,61 @@ require 'stringio'
 
 # Complete the highestValuePalindrome function below.
 def highestValuePalindrome(s, n, k)
-    stringLength = s.length
-    upperLimit = (stringLength/2)-1
-    if upperLimit < 0
-        upperLimit = 0
-    end    
-    # we don't want -ve values for upper limit
-    if stringLength.odd? && s.length > 1
-      upperLimit += 1
+
+  upperLimit = (s.length/2) - 1
+  # this upperLimit excludes the index of the middle character for odd-numbered lengths
+  # the middle character gets special treatment (it does not need to have a 'mirror')
+  if upperLimit < 0
+    upperLimit = 0
+  end
+  # for strings of length 1, the upperLimit will be -ve.
+  # need to set it correctly.  
+  
+  # string sof length > 1 need to be checked if they can be converted to palindromes within k moves
+  # strings of length 1 are already palindromes
+  if (s.length != 1)
+    minNumChanges = 0; # Minimum no. of changes to return a palindrome
+    for i in (0..upperLimit)
+      leftChar = s[i]
+      rightChar = s[s.length-1-i]
+
+      if leftChar != rightChar
+        minNumChanges += 1
+      end    
     end
 
-    numOfChanges = 0
-    for i in (0..upperLimit)
-      if (stringLength.odd? && i == upperLimit)
-        # we have reached the middle of a string of odd-numberred length
-        if (k - numOfChanges) >= 1
-          s[i] = "9"
-          numOfChanges += 1
-        end
-        next    
-      end
-      leftChar = s[i]
-      rightChar = s[stringLength-1-i]
-      
-      if (k - numOfChanges) >= 2
-          s[i] = "9"
-          s[stringLength-1-i] = "9"
-          numOfChanges += 1
-      else
-          if leftChar > rightChar
-          s[stringLength-1-i] = s[i]
-          else  
-          s[i] = s[stringLength-1-i]
-          end
-          numOfChanges += 1
-      end  
-      
-    end
-    if numOfChanges > k
+    if minNumChanges > k
       return "-1"
-    else
-      return s
-    end  
+    end
+  end
+    
+  # TODO: handle middle char in odd string
+  # If we reach here, it's possible to return a palindome...but how to return the
+  # largest palindrome?
+
+  if (s.length == 1 && k >= 1)
+    return "9"
+  elsif (s.length == 1 && k < 1)
+    return "-1"
+  end    
+
+  numChanges = 0
+  for i in (0..upperLimit)
+    leftChar = s[i]
+    rightChar = s[s.length-1-i]
+    
+    if (leftChar != rightChar) && (k - numChanges >= 2)
+      s[i] = "9"
+      s[s.length-1-i] = "9"
+    elsif (leftChar != rightChar) && (k - numChanges == 1)
+      if leftChar > rightChar
+        s[s.length-1-i] = leftChar
+      else
+        s[i] = rightChar
+      end  
+    end
+  end  
+  s
 end
 
 test = File.open("./test.txt")
