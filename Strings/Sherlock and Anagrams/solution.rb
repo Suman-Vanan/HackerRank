71599f57beb7ce28f3213a4683f02f9d6eabc4f0
Suman Vanan <suman.vanan@live.com>
2018-10-13 10:44:15 +0800

    Passes basic test cases, but solution times out for larger test cases

--- Strings/Sherlock and Anagrams/solution.rb
+++ Strings/Sherlock and Anagrams/solution.rb
@@ -9,35 +9,34 @@ def sherlockAndAnagrams(s)
   s.split(//).each { |char| counts[char] += 1 }
 
   result = 0
-  # # special case: handle single-char anagrammatic pairs
-  # counts.each do |char, count|
-  #   if count >= 2
-  #     result += 1
-  #   end
-  # end
-  
-  string_array = s.chars
+
   # all_combinations_array = (1..s.size).flat_map { |n| string_array.combination(n).map(&:join) }.sort
   all_combinations_array = (1..s.length).flat_map { |l| (0..s.length - l).map { |i| s[i, l] } }
-  
-  while !(all_combinations_array.empty?) 
+
+  until all_combinations_array.empty?
     sub_string = all_combinations_array.shift
-    result += all_combinations_array.count(sub_string.reverse)
     # TODO: handle all anagrams, not just the 'sub_string.reverse'
+    # instead of generating all possible permutations of this substring which is O(n!),
+    # let's identify elements in all_combinations_array that contain all the chars in sub_string
+    all_combinations_array.each do |potentialAnagram|
+      if sub_string.chars.sort == potentialAnagram.chars.sort
+        result += 1
+      end
+    end
   end
   result
 end
 
-test_input = File.open("test_input.txt")
+test_input = File.open('test_input.txt')
 
 q = test_input.gets.to_i
 
-q.times do |q_itr|
-    s = test_input.gets.to_s.rstrip
+q.times do |_q_itr|
+  s = test_input.gets.to_s.rstrip
 
-    result = sherlockAndAnagrams s
+  result = sherlockAndAnagrams s
 
-    puts result
+  puts result
 end
 
-test_input.close()
+test_input.close
